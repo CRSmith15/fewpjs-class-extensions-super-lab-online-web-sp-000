636c6db5e64da3911a3015f318173b5e1659c42e
Chance Smith <chancesmith97@hotmail.com>
2020-10-01 02:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ class Deciduous extends Tree {
   constructor(species, name){
     super(species);
     this.name = name;
+  }
 
+  static definition(){
+    return (
+      super.definition() + `Deciduous trees shed their leaves annually.`
+    )
   }
 }
